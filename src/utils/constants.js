5180d818081cf6ecb8f96b4047bb0550f9ef5d99
videne <george.videne@gmail.com>
2026-03-31 21:08:26 +0300

    Create src/utils/constants.js file with demo financial data for AlphaLens

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Sample financial data for AlphaLens
+module.exports = {
+    stockData: [
+        { ticker: 'AAPL', price: 150.00, volume: 1000000, date: '2026-03-31' },
+        { ticker: 'MSFT', price: 250.00, volume: 500000, date: '2026-03-31' },
+        { ticker: 'GOOGL', price: 2800.00, volume: 300000, date: '2026-03-31' },
+        { ticker: 'AMZN', price: 3500.00, volume: 200000, date: '2026-03-31' },
+    ],
+};
