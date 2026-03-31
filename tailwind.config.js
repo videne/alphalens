8ac784a3d2d7ba94ed375409a3c596a699f8ad1a
videne <george.videne@gmail.com>
2026-03-31 21:07:34 +0300

    Create tailwind.config.js with terminal theme configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'terminal-dark': '#1e1e1e',
+        'terminal-green': '#00ff00',
+        'terminal-gold': '#ffd700',
+      },
+      fontFamily: {
+        'fira': ['Fira Code', 'monospace'],
+        'playfair': ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
