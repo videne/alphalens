71523b010014c87233ac919dcaf08a82d5ea0420
videne <george.videne@gmail.com>
2026-03-31 21:15:36 +0300

    Create Footer component with copyright and platform info

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>© 2026 Videne. All rights reserved.</p>
+            <p>Platform: Alphalens</p>
+        </footer>
+    );
+};
+
+export default Footer;
