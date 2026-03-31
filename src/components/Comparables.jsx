0fcf7d28e97987e3d8fe8ce3289fc0643e78091d
videne <george.videne@gmail.com>
2026-03-31 21:13:41 +0300

    Create Comparables component to show peer comparison metrics.

---
+++ src/components/Comparables.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Comparables = () => {
+    return (
+        <div>
+            <h1>Peer Comparison Metrics</h1>
+            {/* Add logic here to display comparison metrics */}
+        </div>
+    );
+};
+
+export default Comparables;
